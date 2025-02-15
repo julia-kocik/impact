cdc08226f1f9abf57fbfdaec456fbcdf3dd6fa96
julia-kocik <julia.kocik@gmail.com>
2025-02-15 14:33:11 +0100

    Add initial structure

---
+++ src/main/java/pl/puzzle/impact/ImpactApplication.java
@@ -0,0 +1,13 @@
+package pl.puzzle.impact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/puzzle/impact/ImpactApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.puzzle.impact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
