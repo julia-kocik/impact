1483a5b8321358338246abd9b8e0584ba82b1919
julia-kocik <julia.kocik@gmail.com>
2025-02-24 21:03:02 +0100

    Add role

--- src/main/java/pl/puzzle/impact/user/CreateUserService.java
+++ src/main/java/pl/puzzle/impact/user/CreateUserService.java
@@ -7,10 +7,12 @@ import pl.puzzle.impact.user.dto.UserCreateDTO;
 public class CreateUserService {
     private final ProfileRepository profileRepository;
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public CreateUserService(ProfileRepository profileRepository, UserRepository userRepository) {
+    public CreateUserService(ProfileRepository profileRepository, UserRepository userRepository, RoleRepository roleRepository) {
         this.profileRepository = profileRepository;
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     public User createUser(UserCreateDTO userCreateDTO) {
@@ -19,6 +21,8 @@ public class CreateUserService {
         userRepository.save(user);
         Profile profile = Profile.createProfile(userCreateDTO, user.getId());
         profileRepository.save(profile);
+        Role role = Role.createRole(user.getId());
+        roleRepository.save(role);
         return user;
     }
 }
---
+++ src/main/java/pl/puzzle/impact/user/dto/RoleCreateDTO.java
@@ -0,0 +1,9 @@
+package pl.puzzle.impact.user.dto;
+
+
+import lombok.Getter;
+
+@Getter
+public class RoleCreateDTO {
+    private String type;
+}
