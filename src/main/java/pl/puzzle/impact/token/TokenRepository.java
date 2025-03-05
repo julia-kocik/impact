33e9b4c961b616092369e46ce7dc0e7d1a4c9f4d
Ivor <tkivor@gmail.com>
2025-03-05 20:43:17 +0100

    Imports reorganized

--- src/main/java/pl/puzzle/impact/token/TokenRepository.java
+++ src/main/java/pl/puzzle/impact/token/TokenRepository.java
@@ -2,7 +2,6 @@ package pl.puzzle.impact.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 import java.util.UUID;
 
--- src/main/java/pl/puzzle/impact/token/TokenService.java
+++ src/main/java/pl/puzzle/impact/token/TokenService.java
@@ -3,7 +3,6 @@ package pl.puzzle.impact.token;
 import org.springframework.stereotype.Service;
 import pl.puzzle.impact.common.exception.TokenNotFoundException;
 import pl.puzzle.impact.token.dto.TokenRequest;
-
 import java.util.Optional;
 import java.util.UUID;
 
